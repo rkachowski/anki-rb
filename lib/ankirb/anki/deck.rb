075cd5f561788bd8fd21be20bed2eb0278b9ff6b
Donald Hutchison <git@toastymofo.net>
2016-01-29 16:52:29 +0100

    Add card to deck.

--- lib/ankirb/anki/deck.rb
+++ lib/ankirb/anki/deck.rb
@@ -1,14 +1,43 @@
 module Anki
   class Deck
-    attr_accessor :name, :id
+    attr_accessor :name
+    attr_reader :id, :cards
 
     def initialize name
       @name = name
       @id = Time.now.to_i
+      @cards = {}
+    end
+
+    def add_card card
+      card.deck = self
+      @cards[card.id] = card
+    end
+
+    def cards
+      @cards.values
+    end
+
+    def [] id
+      @cards[id]
     end
   end
 
   class Card
     attr_accessor :front, :back, :tags, :deck
+    attr_reader :id
+
+    @@defaults = {
+        :front => 'Card Front',
+        :back => 'Card Back'
+    }
+
+    def initialize options={}
+      @front = options[:front] || @@defaults[:front]
+      @back = options[:back] || @@defaults[:back]
+      @tags = options[:tags] || []
+
+      @id = Time.now.to_i
+    end
   end
 end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -2,10 +2,21 @@ require 'test_helper'
 
 class DeckTest < Minitest::Test
   def test_construct_deck
-    deck = Anki::Deck.new "TestDeck"
+    deck = Anki::Deck.new 'TestDeck'
 
-    assert(deck, "deck should exist")
-    assert_equal("TestDeck", deck.name, "name should match")
-    assert_kind_of(Fixnum, deck.id, "id should be a fixnum (int)")
+    assert(deck, 'deck should exist')
+    assert_equal('TestDeck', deck.name, 'name should match')
+    assert_kind_of(Fixnum, deck.id, 'id should be a fixnum (int)')
+  end
+
+  def test_add_card
+    deck = Anki::Deck.new 'TestDeck'
+
+    card = Anki::Card.new
+    deck.add_card card
+
+    assert(deck.cards.include?(card), 'deck should have the card')
+    assert_equal(deck, card.deck, 'deck reference should be set')
+    assert_equal(deck[card.id], card, 'should retrieve card by id')
   end
 end
